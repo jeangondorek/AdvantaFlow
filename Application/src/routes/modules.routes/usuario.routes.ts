f0767f1b8e6ea01b2218c6fdc2826d5597f42a8d
jeangondorek <jeangondorek73@gmail.com>
2023-07-11 16:25:40 +0000

    fix database, fix auth middlewares and fix env, addedded jwt and bcrypt

--- Application/src/routes/modules.routes/usuario.routes.ts
+++ Application/src/routes/modules.routes/usuario.routes.ts
@@ -1,14 +1,14 @@
-import { Router } from "express";
+import { Router } from 'express';
 import { controller }  from '../../imports'
-import { authMiddleware } from "../../middleware/auth.middleware";
+import { authMiddleware } from '../../middleware/auth.adm.middleware';
 
 const routerusuario = Router();
 
-routerusuario.get('/usuario', controller.usuario.getallUsuario);
-routerusuario.post('/usuario', controller.usuario.createUsuario);
-routerusuario.put('/usuario/:cpf_usuario', controller.usuario.updateUsuario);
+routerusuario.get('/usuario', authMiddleware, controller.usuario.getallUsuario);
+routerusuario.post('/usuario', controller.usuario.createUsuario); // TO-DO colocar authmiddleware em abiente de produção. Em dev deixado sem esse fix porque será necessário reiniciar o banco não tendo um usuário fixo como padrão
+routerusuario.put('/usuario/:cpf_usuario', authMiddleware, controller.usuario.updateUsuario);
 routerusuario.get('/usuario/:cpf_usuario', authMiddleware, controller.usuario.getbycpfUsuario);
-routerusuario.delete('/usuario/:cpf_usuario', controller.usuario.deleteUsuario);
+routerusuario.delete('/usuario/:cpf_usuario', authMiddleware, controller.usuario.deleteUsuario);
 routerusuario.post('/login', controller.login.login);
 
 export { routerusuario };
