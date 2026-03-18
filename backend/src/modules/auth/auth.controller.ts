eb875159d05a70f2753ce7639d6a4dccb518e167
jeangondorek <jeangondorek73@gmail.com>
2026-03-17 21:46:50 -0300

    feat: auth controller

---
+++ backend/src/modules/auth/auth.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() body: any) {
+    return this.authService.login(body.email, body.password);
+  }
+}
