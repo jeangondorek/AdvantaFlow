9d076847eea2d016ca51ced7d6e1ea47d47f4969
jeangondorek <jeangondorek73@gmail.com>
2026-03-17 21:46:36 -0300

    feat: users controller

---
+++ backend/src/modules/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() body: any) {
+    return this.usersService.create(body);
+  }
+}
