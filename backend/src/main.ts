327ba1b95ed8aa936cd1c3e06600bdf364466620
jeangondorek <jeangondorek73@gmail.com>
2026-03-22 16:39:11 -0300

    a122354fasasas

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ErrorMiddleware } from './middleware/error.middleware';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new ErrorMiddleware());
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('AdvantaFlow API')
+    .setDescription('Documentação da API AdvantaFlow')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
